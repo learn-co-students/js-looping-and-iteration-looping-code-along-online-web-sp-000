4d6e27089cfa250e10469682a0aa912928e5b8ee
github username <github email address>
2019-05-01 10:08:30 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solutions in this file
+function printBadges(employees){
+  for(let i = 0; i < employees.length; i++){
+      console.log("Welcome" + " " + employees[i] + "! " + "You are employee #" + [i] + ".");
+  }
+     return employees;
+}
+
+// 'Welcome Joe! You are employee #1.'
