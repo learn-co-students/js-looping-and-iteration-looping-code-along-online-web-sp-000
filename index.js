05d2a803687737f198e19ddffc935f9a1bee05c0
github username <github email address>
2019-05-01 12:52:08 -0400

    Done.

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ function printBadges(employees){
      return employees;
 }
 
-function tailsNeverFails(){
-
-  let tails = 0;
-  //
-  while (Math.random() >= 0.5) {
-      tails++;
-   }
-     return `You got ${tails} tails in a row!`;
-}
-
+// function tailsNeverFails(){
+//
+//   let tails = 0;
+//   //
+//   while (Math.random() >= 0.5) {
+//       tails++;
+//    }
+//      return `You got ${tails} tails in a row!`;
+// }
+//
 
 
 
