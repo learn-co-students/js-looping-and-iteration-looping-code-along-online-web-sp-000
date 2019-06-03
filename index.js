80669c984a60c92c6e759fb1f2bf203f6e544ef5
Sharon Alajajian <sharonoptsout@gmail.com>
2019-06-03 15:51:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (nameArray, eventName) {
+  let myArray = []
+  for (let i =0; i<nameArray.length; i++) {
+    myArray.push(`Thank you, ${nameArray[i]}, for the wonderful ${eventName} gift!`);
+  }
+  return myArray;
+}
+
+function countdown(myInteger) {
+  while(myInteger >= 0) {
+    console.log(myInteger);
+    myInteger -=1;
+  }
+}
