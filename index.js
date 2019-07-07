e27eb9cdfbe5b3a6aecb855ca3421df51b86e313
Jeff Benton <sikletz@gmail.com>
2019-07-07 16:02:50 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(arr) {
+  for(let i in arr){
+    arr[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+  }
+  return arr;
+}
+
+function countdown(num) {
+  for(let i = num; i>=0; i--) {
+    console.log(i);
+  }
+}
