cfd2e269fb4079b0b6d3bb8bd9c9e7a3840c6423
blainelawson <blaine.lawson@gmail.com>
2019-07-22 13:25:51 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+    let cardsArray = [];
+
+    for (let i = 0; i < names.length; i++) {
+        cardsArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return cardsArray;
+}
+
+function countdown(anInteger) {
+    while(anInteger >= 0) {
+        console.log(anInteger--);
+    }
+}
