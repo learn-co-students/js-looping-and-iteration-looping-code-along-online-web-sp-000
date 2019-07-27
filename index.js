298a8cbc9ebcaf3d24c2e0a983d6be5eeaffa8b5
Tommy Curley <thomas.curley93@gmail.com>
2019-07-27 17:12:30 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, giftType) {
+    const retArr = [];
+    for (let i = 0; i < names.length; i++) {
+        retArr[i] = `Thank you, ${names[i]}, for the wonderful ${giftType} gift!`;
+    }
+    return retArr;
+}
+
+function countdown(num) {
+    let i = 0;
+    while (i <= num) {
+        console.log(num-i);
+        ++i;
+    }
+}
