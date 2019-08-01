7b1f8878c66c6d56fa4dd41689020a07efda8649
atribecalledarty <arth3rs0ng@gmail.com>
2019-08-01 12:03:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(people, typeOfGift) {
+    const messages = [];
+    for ( let i=0; i < people.length; i++ ) {
+        messages.push(`Thank you, ${people[i]}, for the wonderful ${typeOfGift} gift!`);
+    }
+    return messages;
+}
+
+function countdown(number){
+    for (let i=number; i>=0; i--) {
+        console.log(i);
+    }
+}
