f2ffcdfac3485a6e2feda1c1a86a2c9b8a4d8f0f
Jared Grove <jaredkgrove@gmail.com>
2019-08-20 16:04:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(people, occasion) {
+    const thankYous = []
+    for (let i = 0; i < people.length; i++) {
+        thankYous.push(`Thank you, ${people[i]}, for the wonderful ${occasion} gift!`);
+    }
+ 
+  return thankYous;
+}
+
+function countdown(integer){
+    while (integer >=0 ){
+        console.log(integer)
+        integer --
+    }
+}
+//  "Thank you, Ali, for the wonderful birthday gift."
