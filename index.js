f5a23524c64f36bdd6ac5b1cb1151ca1c1b49c8c
Barbara Johnson <johnson.barbara@gmail.com>
2019-08-20 19:03:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+function writeCards(names, occasion) {
+    const thankYou = [];
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push("Thank you, " + names[i] + ", for the wonderful " + occasion + " gift!");
+    }
+  
+    return thankYou;
+  }
+
+  function countdown(num)
+  {
+      while (num >= 0) 
+      {
+          console.log(num--);
+      }
+  }
