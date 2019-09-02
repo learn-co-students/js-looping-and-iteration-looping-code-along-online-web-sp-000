43b43c9faebf187e5043d497c100e5df3b4be86e
Ray Valdez <ray_valdez@hotmail.co.uk>
2019-09-02 15:50:07 +0100

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(array, b) {
+  let newArray = [];
+  for (let c = 0; c < array.length; c++) {
+    newArray.push(`Thank you, ${array[c]}, for the wonderful ${b} gift!`);
+  }
+  return newArray;
+}
+
+function countdown(n) {
+  let cd = n;
+  while (cd > - 1) {
+    console.log(cd--);
+  }
+}
