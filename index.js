fe53f94510e4dfd474c4bcb727d3481107a0adf8
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-09-11 03:17:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, events){
+  const thanksCards = [];
+  for (let i = 0; i < names.length; i++) {
+    thanksCards.push("Thank you, " + names[i] + ", for the wonderful " + events  + " gift!");
+  }
+  return thanksCards;
+}
+
+function countdown(num)
+{
+    while (num >= 0)
+    {
+        console.log(num--);
+    }
+}
