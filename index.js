aef0913b9ea8827ac1c1b3e6010ef9ed77ccf256
ch2isk4kos <ch2isk4kos@gmail.com>
2019-09-12 21:18:18 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, celebration) {
+
+    let thankYouCards = [];
+
+    for(let i = 0; i < names.length; i++ ) {
+        thankYouCards.push( `Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+    }
+
+    return thankYouCards;
+}
+
+
+function countdown(number) {
+
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+
+    console.log(number);
+}
