e08a4d99a906ec73fac028c9e36ecb650cc344e3
donna <donnacamos88@gmail.com>
2019-09-14 16:36:20 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const gifts = ["teddy bear", "drone", "doll"];
+ 
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+ 
+  return gifts;
+}
+ 
+wrapGifts(gifts);
+
+function wrapGifts(gifts) {
+    let i = 0; // the initialization moved OUTSIDE the body of the loop!
+    while (i < gifts.length) {
+      console.log(`Wrapped ${gifts[i]} and added a bow!`);
+      i++; // the iteration moves INSIDE the body of the loop!
+    }
+   
+    return gifts;
+  }
+   
+  wrapGifts(gifts);
