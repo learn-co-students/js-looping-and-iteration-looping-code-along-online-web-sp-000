500279bb721d3df3c7030e570af6995d06df7843
Aliciea Dawn Suechin <aliciea@Alicieas-MacBook-Pro.local>
2019-09-25 17:18:43 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+let thankYouMessages = [];
+
+function writeCards(namesArray, event) {
+    for (let i = 0; i < namesArray.length; i++) {
+        thankYouMessages.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYouMessages
+}
+
+function countdown(countdown) {
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown -= 1;
+    }
+    console.log(countdown);
+}
