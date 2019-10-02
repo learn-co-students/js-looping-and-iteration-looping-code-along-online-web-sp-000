da553f5d4866bfd8b21735c6e035eabe4389581c
jehough <teachlife@outlook.com>
2019-10-01 22:11:41 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(cards, day) {
+  const array = []
+  for (let i = 0; i < cards.length; i++)
+  {array.push(`Thank you, ${cards[i]}, for the wonderful ${day} gift!`)}
+  return array
+}
+
+
+function countdown(num) {
+  let i = num
+  while (i >= 0){
+    console.log(i)
+    i--
+  }
+}
