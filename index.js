9656a94032862e8c9af1b1fcc9a47273ff94bf1b
Ramon Chavez <rachavez1974@gmail.com>
2019-10-03 00:51:04 -0400

    Finish js-looping-and-iteration-looping-code-along-online-web-sp-000

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(names, eventName){
+  let message = [];
+  for (let  i = 0; i < names.length; i++){
+    message.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+  }
+  return message;
+}
+
+function countdown(number) {
+  for(let i = number; i >= 0; i--){
+    console.log(i);
+  }
+}
