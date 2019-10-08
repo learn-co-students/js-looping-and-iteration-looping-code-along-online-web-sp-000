d346cbc3bd495f5b6a8b4a90c6a86473d3289e35
Bill G <gbilly3@gmail.com>
2019-10-08 20:44:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+//
+// function writeCards(array, event){
+//   ray = []
+// for (let i = 0; i < array.length; i++){
+//
+//   ray.push('Thank you, ${array[i]}, for the wonderful ${event} gift!')
+// }
+// return ray
+// }
+
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+//
+//
+// 
+// function countdown(countdown){
+// // let countdown = 10
+// while (countdown > 0)
+// console.log(countdown--);
+// }
+
+
+
+function countdown( countdown ) {
+  while ( countdown > 0 ) {
+
+    console.log( countdown );
+    countdown -= 1;
+  }
+
+  console.log( countdown );
+}
