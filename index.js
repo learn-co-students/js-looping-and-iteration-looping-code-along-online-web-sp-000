dcf034bf33a14873cd0e313d63d1c40c9217f4e8
Jane Venter <janeanneventer@gmail.com>
2019-10-10 18:34:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const cards = ["Ada", "Brendan", "Ali"];
+function writeCards(cards, birthday) {
+  const newcards = [];
+  for(let i = 0; i < cards.length; i++) {
+    newcards.push(`Thank you, ${cards[i]}, for the wonderful ${birthday} gift!`);
+  }
+  return newcards;
+}
+
+writeCards(cards, birthday);
+
+function countdown(n) {
+  let i = n;
+  while(i >= 0) {
+    console.log(i)
+    i--
+  }
+}
