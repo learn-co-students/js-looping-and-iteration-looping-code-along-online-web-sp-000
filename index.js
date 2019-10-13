5da5203930fd6ee7dce53beba5db30e9e23ff436
LHSharing <daleyluis@gmail.com>
2019-10-12 22:27:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(someArray, eventName) {
+    let thankYouCards = [];
+    for (let i = 0; i < someArray.length; i++) {
+        thankYouCards.push(
+            `Thank you, ${someArray[i]}, for the wonderful ${eventName} gift!`
+        );
+    }
+    return thankYouCards;
+}
+
+function countdown(countdown) {
+    while (countdown > 0) {
+        console.log(countdown);
+        countdown -= 1;
+    }
+    console.log(countdown);
+}
