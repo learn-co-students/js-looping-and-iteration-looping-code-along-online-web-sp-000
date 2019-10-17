a23f6d7d1cb925bca0d6940d3421721830c5fd21
Solomon Feldman <solomonrfeldman@gmail.com>
2019-10-17 02:08:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, holiday) {
+  let messages = []
+  for (let i = 0; i < names.length; i++) {
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${holiday} gift!`)
+  }
+  return messages
+}
+
+function countdown(number) {
+  while (number >= 0) {
+    console.log(number)
+    number--
+  }
+}
