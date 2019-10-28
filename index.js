f6faa8e6935202c6cc6ce4aaf5a06c6176bc7327
Bry Cox <carl.cc@gmail.com>
2019-10-28 12:36:51 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const arr = [];
+
+function writeCards(cards, surprise) {
+  for (let i = 0; i < cards.length; i++) {
+    arr.push(`Thank you, ${cards[i]}, for the wonderful ${surprise} gift!`);
+  }
+
+  return arr;
+}
+
+function countDown(num) {
+  for (let i = num; i > -1; i--) {
+    console.log(num--);
+  }
+
+  return num;
+}
