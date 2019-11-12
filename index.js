99d0a3c2c38984ac1c1b93d50b7ffe3a7b59b909
Kristina Mari Horensky <kristinahorensky@gmail.com>
2019-11-11 20:11:27 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+
+function writeCards(names, eventName) {
+    const arr = [];
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return arr;
+}
+
+
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int --;
+    }
+}
