3a745566999d6f59a440df5aa95936e99a3223f5
morganpstanley <morganpstanley@gmail.com>
2019-11-21 23:54:56 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+function writeCards(givers, reason) {
+    let resultz = [];
+    for (let i = 0; i < givers.length; i++) {
+        resultz.push( `Thank you, ${givers[i]}, for the wonderful ${reason} gift!` );
+    } 
+    return resultz;
+}
+
+function countDown( number ) {
+    for (let i = number; i >= 0; i--) {
+        console.log(i);
+    }
+}
