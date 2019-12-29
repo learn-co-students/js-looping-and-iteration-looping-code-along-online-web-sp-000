fb5c99ba234f429791a7189defe08f6ab4cb839f
Lindsey Wells <github email address>
2019-12-29 20:50:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 // Code your solutions in this file
 
 
-const names = []
-const occassion = "Birthday"
+
 let messages = []
 
 function writeCards(names, occassion) {
     for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${occassion} gift!`);
     }
 
     return messages;
 }
 
 function countDown(number) {
-    for (let i = number; number >= 0; number--) {
+    while (number >= 0) {
         console.log(number);
+        number = number - 1;
     }
 }
