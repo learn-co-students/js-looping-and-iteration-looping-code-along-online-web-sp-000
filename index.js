5de44319d55136189d1d04f940d9aaa570827fec
Mark <m.hern1415@gmail.com>
2020-02-11 13:40:11 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+
+function writeCards(Array, event){ 
+    let newarray = []
+    for (let count = 0; count < Array.length; count++) {newarray.push(`Thank you, ${Array[count]}, for the wonderful ${event} gift!`);
+}
+
+return newarray;
+
+}
+
+
+
+
+function countDown(integer) {
+
+let counter = integer
+
+while (counter >= 0) {
+    console.log(counter)
+    counter-=1
+}
+
+}
