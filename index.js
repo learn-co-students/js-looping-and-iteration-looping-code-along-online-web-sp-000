6081d1d62e1bf4bb8641cc1b2d14c820fd26bc33
Amy Westcott <amy.westcott@yahoo.com>
2020-03-08 21:16:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+   let outputArr = [];
+
+   for (let i = 0; i < array.length; i++) {
+      outputArr.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+      console.log(outputArr)
+   }
+   return outputArr
+}
+
+function countDown(number) {
+   let countdownNumber = number
+
+   while (countdownNumber >= 0) {
+      console.log(countdownNumber)
+      countdownNumber--
+   }
+}
