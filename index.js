aa1915b0a69dbd46516a9a4e3490d086b365cc63
zdsmit <zdsmith001@gmail.com>
2020-03-19 16:12:25 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+
+  let messages = []
+  
+  for (let n=0; n < names.length; n++) {
+    messages.push(`Thank you, ${names[n]}, for the wonderful ${event} gift!`)
+  }
+
+  return messages
+
+}
+
+function countDown(integer) {
+  
+  while (integer >= 0) {
+    console.log(integer--)
+  }
+
+}
