6de94628aa86a315a9702b60f2e1289d7d01593c
Garrett Thompson <gtcruz.gt@gmail.com>
2020-05-15 05:49:23 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfnames, event) {
+  var newArray = []
+  for (let i = 0; i < arrayOfnames.length; i++) {
+    newArray.push('Thank you, ' + arrayOfnames[i] + ", for the wonderful " + event + " gift!");
+  }
+
+  return newArray;
+}
+
+function countDown(num) {
+  while (num > -1){
+    console.log(num);
+    num--
+  }
+}
