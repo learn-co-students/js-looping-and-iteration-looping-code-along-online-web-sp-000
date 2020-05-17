0d5141ba771cc18e793ef954aaeb7b187bdf65b8
andrewsilva9 <andrewsilva9@gmail.com>
2020-05-17 12:53:06 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let new_array = []
+    for (let i = 0; i < array.length; i++) {
+        new_array.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return new_array;
+}
+
+
+function countDown(integer) {
+    while (integer > 0) {
+        console.log(integer);
+        integer -= 1;
+    }
+    console.log(integer);
+}
