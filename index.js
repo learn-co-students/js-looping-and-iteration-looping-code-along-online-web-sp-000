87511a0b4a4be402cce36aa64bada05d9f4c2a8e
Andrew-J-Williams <1williamsaj@gmail.com>
2020-06-03 22:54:52 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let thankYou = [];
+
+    for (let i = 0; i < names.length; i++) {
+        thankYou.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+    
+    return thankYou
+}
+
+function countDown(value) {
+
+    while(value > -1) {
+        console.log(value--)
+    }
+
+}
