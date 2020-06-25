51a7ce345bfc9cea0978ffb15be7268a5e9bb0c5
Firooz Mafakheri <firooz.mafakheri@gmail.com>
2020-06-25 00:16:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function countDown(num){
+  
+  let counter=num;
+  while(counter >= 0){
+   
+     console.log(counter)
+  
+        counter= counter-1;
+     
+  }
+}
+ function writeCards(str,event){
+   
+   let final= str.length-1;
+   let text=[];
+   for(let count=0;count<=final; count+=1){
+     text.push(`Thank you, ${str[count]}, for the wonderful ${event} gift!`)
+     
+   }
+   return text;
+ }
