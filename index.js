17cb4b00c0ae40e6c479deb011eda6e9ec02e3a3
ottocedeno <otto@creativespaceclub.com>
2020-07-26 16:42:35 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let thankYouNotes = [];
+  for (let i = 0; i < names.length; i++) {
+    thankYouNotes.push(
+      `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    );
+  }
+  return thankYouNotes;
+}
+
+const friends = ["Joe", "Toria", "Mac"];
+
+writeCards(friends, "Christmas");
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number);
+    number--;
+  }
+  return number;
+}
