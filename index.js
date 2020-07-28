0f6a31216d035feaed2ab21b7d125c8c9aa2af18
Daniel Pataki <ptamasdaniel92@gmail.com>
2020-07-28 21:59:41 +0200

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards (arr, event_name) {
+    result = []
+    for (let i = 0; i < arr.length; i++ ) {
+        result.push(`Thank you, ${arr[i]}, for the wonderful ${event_name} gift!`)
+    }
+    result
+}
+
+function countDown (i) {
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
