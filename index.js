c9d7cbbbacc3b9519e6e968cff4b06d54d812610
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-07-30 16:16:18 +0000

    Done.

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ function writeCards(string_names, event_name) {
 }
 
 function countDown(number) {
-  while (0 < number) {
-    console.log(`${number}`);
+  while (0 <= number) {
+    console.log(number);
     number--;
   }
 }
