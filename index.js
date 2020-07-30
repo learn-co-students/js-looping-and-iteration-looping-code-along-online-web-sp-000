2f21de847de9cef73c0b0214b1a1f87955aaae14
proactive-analyst-6157 <u.batsaikhan@gmail.com>
2020-07-30 16:00:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(string_names, event_name) {
+  let array = []
+  for (let i=0; i<string_names.length; i++) {
+    let message = `Thank you, ${string_names[i]}, for the wonderful ${event_name} gift!`;
+    array.push(message)
+  }
+  return array
+}
+
+function countDown(number) {
+  let i = 0;
+  while (i <= number) {
+    console.log(`${number}`);
+    number--;
+  }
+}
