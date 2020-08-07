5c543de43e2aa948159b871c89653c662d8c0220
Malcolm Leake <echelon1089@gmail.com>
2020-08-07 19:09:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Code your solutions in this file
+
+const friends = ["Ada", "Brendan", "Ali"];
+
+function writeCards(friends) {
+  for (let i = 0; i < friends.length; i++)
+  {console.log('Thank you, ${friends[i]}, for the wonderful birthday gift!');}
+  return friends;
+}
