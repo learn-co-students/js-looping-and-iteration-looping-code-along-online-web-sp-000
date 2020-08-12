e9268a60bbb17f8778973433c441e83ad905b78f
David Strauch <dmaster18@gmail.com>
2020-08-12 18:00:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let greeting_array = [];
+  for(let i = 0; i < names.length; i++) {
+    let greeting = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    console.log(greeting)
+    greeting_array.push(greeting)
+  }
+  return greeting_array;
+}
+
+
+function countDown(value) {
+  let i = 0;
+  while (value >= i) {
+    console.log(value);
+    value--;
+  }
+}
