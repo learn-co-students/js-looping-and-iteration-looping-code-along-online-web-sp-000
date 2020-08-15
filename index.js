f41db9f0f2e6c61e31c18fc0a63403a488dbc2dd
ksummerill <kaitlynsummerill@gmail.com>
2020-08-15 12:59:19 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const name = []
+
+function writeCards(name, celebration) {
+  for (let i = 0; i < name.length; i++) {
+    console.log(`Thank you, ${name[i]}, for the wonderful surprise gift!`);
+  }
+  return name;
+}
+
+writeCards(name);
+
+
+function countDown(integer) {
+  while (integer < 0) {
+    console.log(integer++);
+  }
+}
