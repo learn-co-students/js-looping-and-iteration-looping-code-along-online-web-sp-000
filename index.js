49e4941fe1a95258ae9175788439c36b53f4da03
agiletkiewicz <a.letkiewicz@gmail.com>
2020-08-25 18:04:52 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let messages = []
+    for (let i = 0; i < array.length; i++) {
+        messages.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return messages;
+}
+
+function countDown(number) {
+    let i = number 
+    while (i >= 0) {
+        console.log(i)
+        i--
+    }
+}
