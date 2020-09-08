493382f98c3ff44de37aa8216d928a82bbb33d08
ting682 <tchung682@gmail.com>
2020-09-08 11:27:26 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+function writeCards(array) {
+    let a = [];
+    for(let i = 0; i < array.length; i++) {
+        a.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+
+    }
+    return a;
+
+}
+
+function countDown(times) {
+    let i = 0;
+    while (i <= times) {
+        console.log(i);
+        i++;
+    }
+
+    return times;
+}
