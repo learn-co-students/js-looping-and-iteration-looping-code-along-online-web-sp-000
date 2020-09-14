1f28bc76c2db8399c8c989108a0c38459ff4a282
github username <github email address>
2020-09-14 18:32:17 -0400

    Done.

--- index.js
+++ index.js
@@ -12,16 +12,8 @@ function writeCards(names, event) {
         return newArray; 
     }
     
-    let countDown = function f(10) {
-        console.log(10);
-    
-        let nextNumber = 10 - 1;
-    
-        if (nextNumber > 0) {
-            f(nextNumber);
-        }
-    }
-    
-    let newCountDown = countDown;
-    countDown = null;
-    newCountDown(10);
+   var countDown = 10; 
+   while (countDown > 0) {
+       console.log(countDown);
+       countDown;
+   }
