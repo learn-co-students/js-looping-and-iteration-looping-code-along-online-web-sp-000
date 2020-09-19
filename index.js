cc7c2cad4951ec2c4688285c324e6e8f0c267caf
github username <github email address>
2020-09-19 19:24:35 -0400

    Done.

--- index.js
+++ index.js
@@ -21,21 +21,13 @@
     let newarray = []
     for (let count = 0; count < Array.length; count++) {newarray.push(`Thank you, ${Array[count]}, for the wonderful ${event} gift!`);
 }
-
 return newarray;
-
 }
 
-
-
-
 function countDown(integer) {
-
 let counter = integer
-
 while (counter >= 0) {
     console.log(counter)
     counter-=1
 }
-
 }
