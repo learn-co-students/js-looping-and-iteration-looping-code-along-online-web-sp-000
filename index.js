9a47c46af750d4b2d269ba5f291d20cfa6a66d51
Oscar Shoudt <shoudto@gmail.com>
2020-10-12 19:23:45 -0400

    Done.

--- index.js
+++ index.js
@@ -2,16 +2,20 @@
 
 
  
-function writeCards(cards, message) {
-  for (let i = 0; i < cards.length; i++) {
-    console.log(`Thank you, ${cards[i]}, for the wonderful ${message} gift!`);
+function writeCards(names, eventName) {
+  var cardArray = [];
+  for (let i = 0; i < names.length; i++) {
+    cardArray.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
   }
  
-  return cards;
+  return cardArray;
 }
+
 writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
 
 
+
+// The Count Down Function
 
 function countDown(number) {
     let counter = 0;
