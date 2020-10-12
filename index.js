256ea4f3c8271e0c732afdf0cd006b203d19bdd2
Oscar Shoudt <shoudto@gmail.com>
2020-10-12 18:56:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+ 
+function writeCards(cards, message) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]}, for the wonderful ${message} gift!`);
+  }
+ 
+  return cards;
+}
+writeCards(["Lisa", "Kaitlin", "Jan"], "surprise");
+
+
+
+function countDown(number) {
+    let counter = 0;
+    while (counter <= number){
+        console.log(number--);
+    }
+}
