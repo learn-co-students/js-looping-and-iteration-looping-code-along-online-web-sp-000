81b71b479699b6b71270c273dca3b4d7106267d8
murphcoder <murphcoder@gmail.com>
2020-10-18 04:50:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(array, event) {
+    let thankYouArray = [];
+    for (let x = 0; x < array.length; x++) {
+        thankYouArray.push(`Thank you, ${array[x]}, for the wonderful ${event} gift!`);
+    }
+    return thankYouArray;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number = number - 1;
+    }
+}
