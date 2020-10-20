720d6cbda7a8e2492a946c04dd8a32614cbd8a87
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-10-20 05:36:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let thanks = []
+function writeCards(names, event) {
+  for (let i=0; i < names.length; i++) {
+    thanks.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  return thanks
+}
+
+function countDown(num) {
+  let i = num
+  while (num >= 0) {
+    console.log(num)
+    num -= 1
+  }
+}
