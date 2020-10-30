a1f32de99a299a48783bf8a2c3ba88c7dc4d9835
Joseph <DeGregorio.Joseph@gmail.com>
2020-10-30 14:25:15 -0400

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 // Code your solutions in this file
 
-
   function writeCards(name, event) {
       let message = []
       for (let i = 0; i < name.length; i++){
           message[i] = `Thank you, ${name[i]}, for the wonderful ${event} gift!`
-
       }
       return message
   }
