d391dbfd0bd0dcfae5e8d62a8a8586ad70403f01
Taishawnk <Taishawnking1@yahoo.com>
2020-11-01 00:49:26 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(name, event) {
+
+let message = []
+
+for(let t=0; t < name.length; t++) {
+    message.push(`Thank you, ${name[t]}, for the wonderful ${event} gift!`);
+
+}
+return message;
+}
+
+function countDown(n){
+    while (n>-1){
+        console.log(n);
+        n--;
+    }
+}
