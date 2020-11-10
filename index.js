23c65cb5d60927152fbef45b8b7da1aadc0ff5e2
satodd1967 <seth.todd@outlook.com>
2020-11-09 18:34:57 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+
+const array = [];
+
+function writeCards(people, holliday) {
+  for (let i = 0; i < people.length; i++) {
+    array.push(`Thank you, ${people[i]}, for the wonderful ${holliday} gift!`);
+  }
+  return array;
+}
+
+function countDown(number) {
+    let count = number;
+    while(count >= 0) {
+        console.log(count--);
+    }
+}
+
