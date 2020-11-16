97153ade19cf8097015cdd3ac5c5936248487876
Meisuleen Kased <meisuleen@gmail.com>
2020-11-16 04:29:42 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 // Code your solutions in this file
-const names = ["Lisa", "Kaitlin", "Jan"];
 
-function writeCards(names){
+//function that accepts 2 arguments : array of names and an event name
+//increment after each loop
+//stop loop after last name printed
+//collect printed messages in an array and then return it
 
+
+function writeCards(['Lisa', 'Kaitlin', 'Jan'], 'birthday'){
   for (let i = 0; i < names.length; i++) {
+    let array = [];
     console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+      //collect thank you cards in array (use .map or foreach)
+    }
+    return array;
   }
-  return names;
-}
+
+
+  // let countDown = 11;
+  // while (countDown > 1) {
+  //   console.log(countDown--);
+  // }
