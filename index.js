9f045271f95b807e95be4c198ecee03792a689df
David Ingram <david@ingramaviation.com>
2020-11-17 10:45:39 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, celebration) {
+    let messages = []
+    for (let i = 0; i < namesArray.length; i++) {
+        messages.push(`Thank you, ${namesArray[i]}, for the wonderful ${celebration} gift!`);
+    }
+
+    return messages
+}
+
+function countDown(n) {
+    while (n > -1) {
+        console.log(n)
+        n--
+    }
+}
