c2b85834f2cf29df311f4988ccb235e8ee55edf9
Gearz <shanegeary@yahoo.com>
2020-11-26 16:44:47 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(people, event) {
+    const thankYou = [];
+    for (let i = 0; i < people.length; i++) {
+        thankYou.push(`Thank you, ${people[i]}, for the wonderful ${event} gift!`);
+    }
+    return thankYou;
+}
+
+function countDown(number) {
+    let countUp = number;
+    while (countUp >= 0) {
+        console.log(countUp--)
+    }
+}
