cbade04606d0f5d853b24fed0adcfb3725ad917c
Mike Barberry <mikebarberry@protonmail.com>
2020-11-29 05:19:22 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+writeCards = (list, reason) => {
+  let thankYous = []
+  for (let i = 0; i < list.length; i++) {
+    thankYous.push(`Thank you, ${list[i]}, for the wonderful ${reason} gift!`)
+  }
+  return thankYous
+}
+
+function countDown (num) {
+  while (num > 0) {
+    console.log(num);
+    num -= 1;
+  }
+  console.log(num)
+}
+
+countDown(13)
