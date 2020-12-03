564268907118d8c7647ec915a0cb7a416bae52eb
Joseph Perez <hello@josephdanielperez.com>
2020-12-02 22:34:01 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(names, event){
+    let array = []
+    for(let i=0; i<names.length; i++){
+        array.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return array;
+}
+
+function countDown() {
+    let i = 10;
+    while (i > -1) {
+        console.log(i--);
+    }
+}
