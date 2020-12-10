869885659f77a34222998ca0b5184e18853fff70
TrueC-D <aletheacary@gmail.com>
2020-12-10 08:12:15 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(names, type){ 
+   let ThankYouCards = []
+   for (let i= 0; i < names.length; i++)
+     {ThankYouCards.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+    }
+    return ThankYouCards
+}
+
+function countDown(i) {
+    while (i >= 0) {
+        console.log(i--); 
+    }
+}
