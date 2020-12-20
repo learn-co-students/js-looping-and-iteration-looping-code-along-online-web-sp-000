3a4a3fc6f65c2bab6340279139ff3214553c68c3
RickLearnsToCode <rick.rosetti@gmail.com>
2020-12-20 08:05:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, occasion){
+    let returnArray = [];
+    for (let i = 0; i < names.length; i++){
+        returnArray.push("Thank you, " + names[i] + ", for the wonderful " + occasion + " gift!");
+    }
+    return returnArray;
+}
+
+function countDown(startingNumber){
+    let i = startingNumber;
+    while (i >= 0){
+        console.log(i);
+        i--;
+    }
+}
