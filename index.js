9895a4f1d633b3685916ae1f2d323089283a4ffc
SVR <samuelvanrourke@gmail.com>
2021-01-03 14:54:03 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(arr, event) {
+    let output_messages = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        output_messages.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`)
+    }
+    return output_messages
+}
+
+function countDown(n) {
+    let current_count = n;
+
+    while (current_count >= 0) {
+        console.log(current_count);
+        current_count --;
+    }
+
+}
