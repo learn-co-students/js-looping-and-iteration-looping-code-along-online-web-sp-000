e72907e6af9aa2fadd1c93ffdfb249cd98b5c048
reevesalic <reevesalic@gmail.com>
2021-01-10 15:26:26 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+// const array = (["Lisa", "Kaitlin", "Jan"], "birthday");
+
+function writeCards( array, event ) {
+     let thankYouCards = []
+     for ( let i = 0; i < array.length; i++ ) {
+       thankYouCards.push( `Thank you, ${array[i]}, for the wonderful ${event} gift!` )
+     }
+     return thankYouCards
+   }
+
+// let count = 10;
+function countDown(count) {
+     while (count >= 0)     {
+          console.log(count);
+     count -= 1;
+}
+   }   
+  
+//    console.log(count);
