9b8df083a61e25ddf74fcf6490c5cdb7a26fae80
AustinRhoads <austinrrhoads@ymail.com>
2021-01-13 20:35:12 -0600

    how bout make it challenging next time

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown(x){
+    let i = x;
+    while ( i >= 0){
+        console.log(i--);
+    }
+}
+
+function writeCards(ary, word){
+    let newAry = []
+    for(let x = 0; x < ary.length; x++){
+        newAry.push(`Thank you, ${ary[x]}, for the wonderful ${word} gift!`);
+    }
+    return newAry;
+}
