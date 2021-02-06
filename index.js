2361c3c44f64b2cac392653f2c519d47c738580e
john million <jpmillion@comcast.net>
2021-02-06 17:38:38 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(names, eventName) {
+    const messages = ["Thank you, Lisa, for the wonderful surprise gift!", "Thank you, Kaitlin, for the wonderful surprise gift!", "Thank you, Jan, for the wonderful surprise gift!"];
+    for (let i = 0; i < names; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${eventName} gift!`);
+    }
+    return messages;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+    return number;
+}
