8e54f5d2501219a606e0be1d49dd680154bb2145
Carrie Mathieu <carrieburks@gmail.com>
2021-03-11 15:31:10 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let thankYous = []
+function writeCards(names, celebration) {
+    for (let i = 0; i < names.length; i++) {
+        thankYous.push(`Thank you, ${names[i]}, for the wonderful ${celebration} gift!`);
+    }
+    return thankYous
+}
+
+function countDown(n) {
+    let i = 0;
+    while (i <= n) {
+        console.log(i);
+        i++;
+    }
+}
