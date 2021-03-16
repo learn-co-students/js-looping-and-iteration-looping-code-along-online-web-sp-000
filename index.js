63156774b50740f3c9ffd860b2b838ebb5e56d73
nlanese21 <nlanese21@gmail.com>
2021-03-16 02:37:54 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(array, occasion) {
+    const nameArray = array
+    let msg1 = "Thank you, ";
+    let msg2 = ", for the wonderful " + occasion + " gift!";
+    let r_array = [];
+    for (let i = 0; i < nameArray.length; i++){
+        let msg3 = nameArray[i];
+        let full_msg = msg1 + msg3 + msg2;
+        r_array.push(full_msg);
+    }
+    return r_array
+}
+
+function countDown(num){
+    let i = num;
+    while (i >= 0){
+        console.log(i);
+        i = i - 1;
+    }
+}
+
+countDown(8)
