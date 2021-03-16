07f7470c4ebaf59ced97b37e86d073fd281f9bb6
RSigmaSG <sg.comparch@gmail.com>
2021-03-15 20:06:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function countDown() {
+    let countdown = 10;
+    while (countdown >= 0) {
+    console.log(countdown--);
+    }
+}
+function writeCards(names, event) {
+    let out_arr = []
+    for (let i = 0; i < names.length; i++) {
+        out_arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+        console.log(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return out_arr
+}
