1a14a663868d1855a4b4d0d766a269b214659237
Sonja Parsell <hello@sparsell.com>
2021-03-17 06:51:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+
+function writeCards(names, occasion) {
+
+    // const names = ["Lisa", "Kaitlin", "Jan"];
+    // const occasion = ["surprise"];
+    let array = [];
+    for (let i = 0; i < names.length; i++) {
+        array.push(`Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return array;
+}
+
+function countDown(number) {
+    
+    while (number > -1) {
+    console.log(number);
+    number --;
+    }
+}
