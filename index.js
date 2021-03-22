bb41e6c789e72dfd301cdbc76f8023dd80f1da7b
dmepilot <dmepilot@yahoo.com>
2021-03-22 13:46:03 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, occasion) {
+    let array = []
+    for (let i = 0; i < names.length; i++ ){
+        array.push( `Thank you, ${names[i]}, for the wonderful ${occasion} gift!`)
+    }
+    return array
+
+}
+
+function countDown(number) {
+while (number >= 0) {
+  console.log(number--);
+}
+}
