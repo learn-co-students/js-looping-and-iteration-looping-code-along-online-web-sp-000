8ecbe44ba2e4e4d98d6112a212173d5ab8d3b4a4
Anna Guerrero <acg2176@columbia.edu>
2021-03-30 11:37:43 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(cards, event) {
+    const greetings = [];
+    for (let i = 0; i < cards.length; i++) {
+    greetings.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+    }
+return greetings;
+}
+
+writeCards(["Lisa", "Kaitlin", "Jan"])
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num);
+        num--;
+    }
+}
+countDown(10);
