f66f3fb9a656376b3c66daae9a8f1e56331da92d
kkirby16 <kkirby16@my.whitworth.edu>
2021-04-09 18:26:30 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function writeCards(array, event) { 
+    var messages_array = [];
+    for (let i = 0; i < array.length; i++) { 
+    messages_array[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
+    }    
+    return messages_array
+}
+    
+
+function countDown(integer) { 
+    let i = integer; 
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
