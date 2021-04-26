0289cec4ee307ce0762720fe9ee92a5aff29fa43
asb1512 <andrew@olympicmedia.com>
2021-04-25 22:20:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(namesArray, eventName) {
+   let results = [];
+   for (let i = 0; i < namesArray.length; i++) {
+      results.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+   }
+   return results;
+}
+
+function countDown(postiveInteger) {
+   while (postiveInteger > 0) {
+      console.log(postiveInteger);
+      postiveInteger -= 1;
+   }
+   console.log(postiveInteger)
+}
