cf32fd9931e67d786dc4d00ce07249db146d8c66
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2021-05-03 17:25:46 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solutions in this file
+
+const cards = (["Lisa", "Kaitlin", "Jan"]);
+ 
+function writeCards(cards, event) {
+  let result = []
+  for (let i = 0; i < cards.length; i++) {
+    result.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+  }
+  return result
+}
+
+function countDown(fromNumber) {
+    console.log(fromNumber);
+
+    let nextNumber = fromNumber - 1;
+
+    if (nextNumber >= 0) {
+        countDown(nextNumber);
+    }
+}
+countDown(10);
+ 
+
+
