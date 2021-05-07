cd74587bf67cad30c0d2accba0a85b9f9f4d5b84
Nat <ntaktach@gmail.com>
2021-05-07 13:56:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solutions in this file
+// function writeCards(namesArray, event) { 
+//     let thankYouCards = []
+//     for (let i = 0; 1 < namesArray.length; i++) {
+//         thankYouCards.push(`Thank you, ${namesArray[i]}, for the wonderful ${event} gift!`)
+//     }
+//     return thankYouCards
+// }
+
+
+// function countDown(countdown) {
+//     while (countdown > 0) {
+//         console.log(countdown);
+//         countdown -=1;
+//     }
+//     console.log(countdown);
+// } 
+function writeCards( namesArray, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < namesArray.length; i++ ) {
+      thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+  
+  function countDown( countdown ) {
+    while ( countdown > 0 ) {
+      console.log(countdown);
+      countdown--;
+    }
+    console.log(countdown)
+  }
