70784d51b275dd24c50e5e5085395def82349b8e
dbrandt1990 <dvbrandt90@gmail.com>
2021-05-11 13:29:18 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(array, event) {
+    let thankYous = []
+    for (let i = 0; i < array.length; i++) {
+        thankYous.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`)
+    }
+    return thankYous
+}
+
+function countDown(num) {
+    for (let i = num; i > -1; i--) {
+        console.log(i)
+    }
+}
