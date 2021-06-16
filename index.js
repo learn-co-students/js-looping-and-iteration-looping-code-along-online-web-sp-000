34f547ee6cdf008c2ab66c8e0196082d188acd94
Technicallyaustin <Technicallyaustin@gmail.com>
2021-06-16 09:31:06 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+function writeCards(names) {
+    let i = 0;
+    const namesArray = [];
+    while ( i < names.length) {
+        namesArray.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        i++
+    }
+    return namesArray;
+}
+
+function countDown(){
+    let i = 0;
+    while (i < 11){
+        console.log(i);
+        i++
+    } 
+}
