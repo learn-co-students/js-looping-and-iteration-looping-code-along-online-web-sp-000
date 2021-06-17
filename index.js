4178d7ca8e99ba0f3e0840e6c6b76407c5d62927
Ben Hayes <b.t.hayes@comcast.net>
2021-06-17 16:26:36 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+let thankYous = []
+
+function writeCards(names, type) {
+  for (let i = 0; i < names.length; i++) {
+    thankYous[i] = `Thank you, ${names[i]}, for the wonderful ${type} gift!`;
+  }
+
+  return thankYous
+}
+
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
